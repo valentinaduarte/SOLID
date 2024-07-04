c3dc8cf9552fec79bec0993489e9d467a57a0403
valentinaduarte <valentinadc1@hotmail.com>
2024-07-04 10:06:43 -0500

    Updating exercises

--- src/ejercicio5/PayrollService.java
+++ src/ejercicio5/PayrollService.java
@@ -1,8 +1,6 @@
 package ejercicio5;
 
-public class PayrollService {
-
-    
+public class PayrollService { 
     public void calculatePay(Employee employee) {
         // Lógica para calcular el salario
     }
